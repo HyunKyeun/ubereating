ea5d95c66681036d837a785400e4454d1b09e0fd
HyunKyeun <dango11371137@gmail.com>
2024-01-11 15:02:24 +0900

    a

--- nuber-eats-backend/src/restaurants/entities/restaurant.entity.ts
+++ nuber-eats-backend/src/restaurants/entities/restaurant.entity.ts
@@ -1,13 +1,28 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
   @Field(() => String)
+  @Column()
+  @IsString()
   name: string;
-  @Field(() => Boolean)
+
+  @Field(() => Boolean, { nullable: true })
+  @Column({ default: true })
+  @IsOptional()
+  @IsBoolean()
   isVegan: boolean;
-  @Field(() => String)
+
+  @Field(() => String, { defaultValue: 'basic address' })
+  @Column()
+  @IsString()
   address: string;
-  @Field(() => String)
-  ownersName: string;
 }
--- nuber-eats-backend/src/restaurants/restaurants.module.ts
+++ nuber-eats-backend/src/restaurants/restaurants.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RestaurantResovler } from './restaurants.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantsService } from './restaurants.service';
 
 @Module({
-  providers: [RestaurantResovler],
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RestaurantResovler, RestaurantsService],
 })
 export class RestaurantsModule {}
