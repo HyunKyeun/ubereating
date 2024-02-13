4c83b5642963d66012639da68eafc87f8a6ee68f
HyunKyeun <dango11371137@gmail.com>
2024-02-13 11:26:43 +0900

    a

--- nuber-eats-frontend/tailwind.config.js
+++ nuber-eats-frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
